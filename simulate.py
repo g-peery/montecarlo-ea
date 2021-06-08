156f477d0e443adb03740097cd67ed97b88f0fa6
Gabriel Peery <gpeery@uoregon.edu>
2021-06-07 17:36:12 -0700

    Skeleton mostly complete

--- simulate.py
+++ simulate.py
@@ -125,7 +125,7 @@ def _init_rand_states(statesA : np.ndarray, statesB : np.ndarray):
                     stateB[x][y][z]["i"] = rand_binding[1][2][x][y][z]
 
 
-# Trying experimental parallelization
+# Trying parallelization
 @njit(parallel=True)
 def _calc_energies(
         stA : np.ndarray,
@@ -157,7 +157,59 @@ def _calc_energies(
                     ))
 
 
-# Trying experimental parallelization
+# Trying parallelization
+@njit(parallel=True)
+def _calc_overlaps(
+        stA : np.ndarray,
+        stB : np.ndarray,
+        spin_overlap : np.ndarray,
+        link_overlap : np.ndarray
+    ):
+    """For each state in stA and stB are the same size, calculate the 
+    overlaps index by index.
+    
+    Effects:
+        Modifies supplied overlap arrays.
+    """
+    size = len(states[0])
+    for i in prange(len(stA)):
+        spin_overlap[i] = link_overlap[i] = 0.0
+        for x in prange(size):
+            for y in prange(size):
+                for z in prange(size):
+                    spin_overlap[i] += (
+                        stA[i][x][y][z]["s"] * stB[i][x][y][z]["s"]
+                    )
+                    # TODO - Replace with _calc_ss_overlap call?
+                    link_overlap[i] += (
+                        stA[i][x][y][z]["s"] * stB[i][x][y][z]["s"] * (
+                        stA[i][(x+1)%size][y][z]["s"]*stB[i][(x+1)%size][y][z]
+                        + stA[i][x][(y+1)%size][z]["s"]*stB[i][x][(y+1)%size][z]
+                        + stA[i][x][y][(z+1)%size]["s"]*stB[i][x][y][(z+1)%size]
+                        )
+                    )
+
+
+@njit
+def _calc_ss_overlap(
+        stateA : np.ndarray,
+        stateB : np.ndarray,
+        c : np.ndarray
+    ) -> float:
+    """Calculates the contribution of a single site to the link overlap.
+    Returns that contribution. c is the coordinate.
+    """
+    s = len(stateA)
+    return (
+        stateA[c[0]][c[1]][c[2]]["s"] * stateB[c[0]][c[1]][c[2]]["s"] * (
+        stateA[(c[0]+1)%s][c[1]][c[2]]["s"]*stateB[(c[0]+1)%s][c[1]][c[2]]
+        + stateA[c[0]][(c[1]+1)%s][c[2]]["s"]*stateB[c[0]][(c[1]+1)%s][c[2]]
+        + stateA[c[0]][c[1]][(c[2]+1)%s]["s"]*stateB[c[0]][c[1]][(c[2]+1)%s]
+        )
+    )
+
+
+# Trying parallelization
 @njit(parallel=True)
 def _mc_step(
         stA : np.ndarray,
@@ -170,23 +222,37 @@ def _mc_step(
         do_swaps : np.ndarray,
         changes : np.ndarray,
         energiesA : np.ndarray,
-        energiesB : np.ndarray
+        energiesB : np.ndarray,
+        spin_overlap : np.ndarray,
+        link_overlap : np.ndarray,
+        site_overlap : np.ndarray
     ):
     """Given states, information to get their temperatures, and some
     random values to use, performs a Metropolis-Hastings Monte Carlo
-    step on all of them and updates energiesA and energiesB accordingly.
-    The cs array is of random coordinates. Also requires arrays to keep 
-    booleans and changes in energy for parallelization.
+    step on all of them and updates quantities accordingly. The cs array 
+    is of random coordinates. Also requires arrays to keep booleans, 
+    changes in energy, and site_overlap for parallelization.
 
     Effects:
         May swap spins in states.
-        Updates energiesA and energiesB.
+        Updates energiesA, energiesB, spin_overlap, link_overlap.
+        Modifies parallelization utility arrays
     """
     # Loop over state indices
     for sti in prange(len(stA)):
+        # Record old overlap in neighborhood of both coords
+        coords_same = (cs[0][sti] == cs[1][sti]).all()
+        if coords_same:
+            site_overlap[sti] = _calc_ss_overlap(stA[sti], stB[sti], cs[0][sti])
+        else:
+            site_overlap[sti] = (
+                _calc_ss_overlap(stA[sti], stB[sti], cs[0][sti])
+                + _calc_ss_overlap(stA[sti], stB[sti], cs[1][sti])
+            )
+
         # Check if non-positive
         changes[0][sti] = _calc_change(stA[sti], cs[0][sti])
-        changes[1][sti] = _calc_change(stA[sti], cs[0][sti])
+        changes[1][sti] = _calc_change(stB[sti], cs[0][sti])
         do_swaps[0][sti] = (changes[0][sti] <= 0)
         do_swaps[1][sti] = (changes[1][sti] <= 0)
 
@@ -201,13 +267,39 @@ def _mc_step(
             ))
 
         # Perform swaps, update energy info
-        # TODO - can overlaps be updated here?
         if do_swaps[0][sti]:
             stA[sti][cs[0][sti][0]][cs[0][sti][1]][cs[0][sti][2]]["s"] *= -1
             energiesA[sti] += changes[0][sti]
         if do_swaps[1][sti]:
             stB[sti][cs[1][sti][0]][cs[1][sti][1]][cs[1][sti][2]]["s"] *= -1
             energiesB[sti] += changes[1][sti]
+
+        # Update overlaps
+        if coords_same:
+            if not (do_swaps[0][sti] and do_swaps[1][sti]):
+                spin_overlap[sti] -= 2 * (
+                    stB[sti][cs[1][sti][0]][cs[1][sti][1]][cs[1][sti][2]]["s"]
+                    *stA[sti][cs[0][sti][0]][cs[0][sti][1]][cs[0][sti][2]]["s"] 
+                )
+            link_overlap[sti] += (
+                _calc_ss_overlap(stA[sti], stB[sti], cs[0][sti])
+                - site_overlap[sti]
+            )
+        else:
+            if do_swaps[0][sti]:
+                spin_overlap[sti] -= 2 * (
+                    stA[sti][cs[0][sti][0]][cs[0][sti][1]][cs[0][sti][2]]["s"]
+                    *stB[sti][cs[0][sti][0]][cs[0][sti][1]][cs[0][sti][2]]["s"] 
+                )
+            if do_swaps[1][sti]:
+                spin_overlap[sti] -= 2 * (
+                    stA[sti][cs[1][sti][0]][cs[1][sti][1]][cs[1][sti][2]]["s"]
+                    *stB[sti][cs[1][sti][0]][cs[1][sti][1]][cs[1][sti][2]]["s"] 
+                )
+            link_overlap[sti] += ((
+                _calc_ss_overlap(stA[sti], stB[sti], cs[0][sti])
+                + _calc_ss_overlap(stA[sti], stB[sti], cs[1][sti])
+            ) - site_overlap[sti])
 
 
 @time_logging.print_time
@@ -219,7 +311,7 @@ def ptmc(
         betas : np.ndarray,
         global_move_period : int,
         equilibriate_sweeps : int
-        ) -> np.ndarray:
+    ) -> Tuple[np.ndarray, np.ndarray]:
     """Runs a Parallel-Tempering Monte Carlo simulation of an
     edwards_anderson model in a 3D cube with edge lengths size. samples
     is the number of times binding energies are recalculated and
@@ -229,7 +321,11 @@ def ptmc(
     sweeps steps, and an attempt is made every global_move_period to 
     exchange adjacent betas of the systems. Also, the sweeps are allowed 
     to reach equilibrium for some number of sweeps.
+
+    Returns some arrays with encountered values of spin and link overlap
     """
+    # TODO - Replace A/B variables with additional dimension?
+    # TODO - Make overlaps temperature based
     #
     # Memory allocation, startup
     #
@@ -242,14 +338,17 @@ def ptmc(
         dtype=_STATE_DTYPE
     )
 
-    # Store the indices into betas of where to get current
-    # beta. Stands for Temperature _ Relocation
+    # Store the indices into betas of where to get beta of state at some
+    # index. Stands for Beta _ Relocation
     betaA_reloc = np.arange(len(betas), dtype=IDX_DTYPE)
     betaB_reloc = np.arange(len(betas), dtype=IDX_DTYPE)
 
-    # Will initialize energies in loop
+    # Will initialize quantities of interest in loop
     energiesA = np.zeros(len(betas), dtype=BINDING_DTYPE)
     energiesB = np.zeros(len(betas), dtype=BINDING_DTYPE)
+    # Note that for overlaps, we'll save scaling for later
+    spin_overlap = np.zeros(len(betas), dtype=BINDING_DTYPE)
+    link_overlap = np.zeros(len(betas), dtype=BINDING_DTYPE)
 
     # Logistics, counts, useful variables
     do_swaps = np.zeros((2, len(betas)), dtype=bool)
@@ -277,6 +376,12 @@ def ptmc(
     swap_uniforms = np.random.rand(swap_shape)
     print("...finished randoms.")
 
+    # Output quantities of interest
+    # Note that they are stored according to temperature (TODO - broken)
+    # and sample index and sweep index for multiple layers of averaging.
+    spin_overlap_hist = np.zeros((len(betas), samples, sweeps))
+    link_overlap_hist = np.zeros((len(betas), samples, sweeps))
+
     #
     # Sampling loop
     #
@@ -289,6 +394,7 @@ def ptmc(
         #
         _init_rand_states(statesA, statesB)
         _calc_energies(statesA, statesB, energiesA, energiesB)
+        _calc_overlaps(statesA, statesB, spin_overlap, link_overlap)
 
         #
         # Sweeping loop
@@ -307,19 +413,28 @@ def ptmc(
                     do_swaps,
                     changes,
                     energies_A,
-                    energies_B
+                    energies_B,
+                    spin_overlap,
+                    link_overlap
                 )
 
             # Record data after equilibrium phase
             if swi >= real_eq_sweeps:
-                # TODO
-                pass
+                # Record overlaps for histogram later
+                for beta_idx in range(len(betas)):
+                    spin_overlap_hist[betaA_reloc[beta_idx]][sai][swi] = (
+                        spin_overlap[betaA_reloc[beta_idx]]
+                    )
+                    link_overlap_hist[betaA_reloc[beta_idx]][sai][swi] = (
+                        link_overlap[betaA_reloc[beta_idx]]
+                    )
 
             # Periodic Temperature Swap
             quo, rem = np.divmod(swi, global_move_period)
             if rem == 0:
                 # Attempt swaps equal to number of betas
                 for swap_trial in range(len(betas)):
+                    # -=- A -=-
                     # Choose a pair to swap
                     swap_idx = swap_idxs[sai][quo][swap_trial][0]
                     low_i = np.where(betaA_reloc==swap_idx)[0][0]
@@ -335,6 +450,24 @@ def ptmc(
                         betaA_reloc[low_i], betaA_reloc[high_i] = (
                             betaA_reloc[high_i],
                             betaA_reloc[low_i]
+                        )
+
+                    # -=- B -=-
+                    # Choose a pair to swap
+                    swap_idx = swap_idxs[sai][quo][swap_trial][1]
+                    low_i = np.where(betaB_reloc==swap_idx)[0][0]
+                    high_i = np.where(betaB_reloc==(swap_idx+1))[0][0]
+                    # Accept or reject based on boltzmann factor
+                    exponent = (
+                        -energiesB[low_i]*betas[betaB_reloc[high_i]]
+                        -energiesB[high_i]*betas[betaB_reloc[low_i]]
+                        +energiesB[low_i]*betas[betaB_reloc[low_i]]
+                        +energiesB[high_i]*betas[betaB_reloc[high_i]]
+                    )
+                    if (exponent >= 1) or (swap_uniforms <= np.exp(exponent)):
+                        betaB_reloc[low_i], betaB_reloc[high_i] = (
+                            betaB_reloc[high_i],
+                            betaB_reloc[low_i]
                         )
 
         print("...finished sample %i." % sai)
